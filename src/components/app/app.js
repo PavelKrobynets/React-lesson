a7eb799873b54b3e04134362f7f5960d5a77f7b6
Pavel <krobynetspavel@gmail.com>
2024-07-11 17:43:02 +0300

    working with form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,14 +2,14 @@ import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
-import EmployeesAddForm from "../employees-add-form/employees-add-form";
+import { EmployeesAddForm } from "../employees-add-form/employees-add-form";
 import "./app.css";
 
 function App() {
   const data = [
-    { name: "John S.", salary: 800 , increase:false, like: true, id: 1},
-    { name: "Alex P.", salary: 1200 , increase:true, like: false, id: 2},
-    { name: "Bob L.", salary: 500 , increase:false, like: false, id: 3},
+    { name: "John S.", salary: 800, increase: false, like: true, id: 1 },
+    { name: "Alex P.", salary: 1200, increase: true, like: false, id: 2 },
+    { name: "Bob L.", salary: 500, increase: false, like: false, id: 3 },
   ];
 
   return (
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,16 +1,48 @@
-import './employees-add-form.css';
+import "./employees-add-form.css";
+import { Component } from "react";
 
-const EmployeesAddForm = () => {
-	return (
-		<div className="app-add-form">
-			<h3>Add new employee</h3>
-			<form className="add-form d-flex">
-				<input type="text" placeholder='Add name' className="form-control new-post-label"/>
-				<input type="number" placeholder='Payment in $' className="form-control new-post-label" />
-				<button type="submit" className="btn btn-outline-light">Add</button>
-			</form>
-		</div>
-	)
+export class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Add new employee</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            placeholder="Add name"
+            className="form-control new-post-label"
+            onChange={this.onValueChange}
+            name="name"
+            value={name}
+          />
+          <input
+            type="number"
+            placeholder="Payment in $"
+            className="form-control new-post-label"
+            onChange={this.onValueChange}
+            name="salary"
+            value={salary}
+          />
+          <button type="submit" className="btn btn-outline-light">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default EmployeesAddForm;
